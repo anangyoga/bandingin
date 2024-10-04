f21716eb8e99fd793ecfdf595c8729d173e56183
anangyoga <aharwinto@gmail.com>
2024-10-04 20:17:34 +0700

    initialize project & cleanup

---
+++ README.md
@@ -0,0 +1,19 @@
+First, fork this repository. Try this--> [how to fork and clone your forked repo](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/working-with-forks/fork-a-repo#forking-a-repository)
+
+## Getting Started
+
+Once forked and cloned, run this:
+
+```bash
+npm i
+```
+
+Then run your development server:
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+Yok, bisa yok!
---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="mx-auto flex max-w-md flex-col space-y-4 px-3 py-10 sm:px-0">
+      <h1 className="text-center text-xl font-medium text-green-500">
+        Ban Dingin
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nobis
+        assumenda iure sit deserunt quod culpa illo, iste velit quo dignissimos,
+        possimus voluptatibus at perspiciatis qui modi maxime non? Perferendis!
+      </p>
+    </main>
+  );
+}
