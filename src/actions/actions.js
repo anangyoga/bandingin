003cfd861e0f5c38f057fcb5db7e03f15806c408
anangyoga <aharwinto@gmail.com>
2024-10-08 14:00:33 +0700

    feat: action for addProduct

---
+++ src/actions/actions.js
@@ -0,0 +1,43 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+
+export async function addProduct(prevState, formData) {
+  const {
+    name,
+    price,
+    dimension,
+    display,
+    chipset,
+    memory,
+    battery,
+    front_camera,
+    rear_camera,
+  } = {
+    name: formData.get("name"),
+    price: parseInt(formData.get("price")),
+    dimension: formData.get("dimension"),
+    display: formData.get("display"),
+    chipset: formData.get("chipset"),
+    memory: parseInt(formData.get("memory")),
+    battery: parseInt(formData.get("battery")),
+    front_camera: parseInt(formData.get("front_camera")),
+    rear_camera: parseInt(formData.get("rear_camera")),
+  };
+
+  const newProduct = await prisma.product.create({
+    data: {
+      name,
+      price,
+      dimension,
+      display,
+      chipset,
+      memory,
+      battery,
+      front_camera,
+      rear_camera,
+    },
+  });
+
+  return newProduct;
+}
