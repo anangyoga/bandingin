c133dea66bce17b6670e6505b37a505a5796bb6a
anangyoga <aharwinto@gmail.com>
2024-10-09 22:23:08 +0700

    fix: refactor click functionality

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,11 +1,13 @@
+"use client";
+
 import { loginWithGoogle } from "@/actions/auth/sign.google";
 
 export default function Page() {
   return (
     <main className="flex h-screen items-center justify-center">
       <div className="w-[280px] space-y-5 rounded-lg border text-center">
-        <section className="space-y-3">
-          <h1 className="font-bold">Login</h1>
+        <h1 className="font-bold">Login</h1>
+        <form className="space-y-3">
           <div className="grid grid-cols-1 place-items-start">
             <label className="">Email</label>
             <input
@@ -23,12 +25,14 @@ export default function Page() {
           <button className="mx-auto flex items-center justify-center gap-x-2 rounded-lg border border-transparent bg-blue-600 px-3 py-2 text-sm font-medium text-white hover:opacity-90 focus:bg-blue-700 focus:outline-none disabled:pointer-events-none disabled:opacity-50 sm:w-1/5">
             Login
           </button>
-        </section>
-        <form action={loginWithGoogle}>
-          <button className="font-bold text-blue-600">
-            Sign in with Google
-          </button>
         </form>
+
+        <button
+          onClick={() => loginWithGoogle()}
+          className="font-bold text-blue-600"
+        >
+          Sign in with Google
+        </button>
       </div>
     </main>
   );
