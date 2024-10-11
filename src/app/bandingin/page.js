5d3f7396c773e6825dd2fbd7f31223c630c56e18
anangyoga <aharwinto@gmail.com>
2024-10-11 23:26:39 +0700

    fix: layouting & copywriting

--- src/app/bandingin/page.js
+++ src/app/bandingin/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Page = () => {
   return (
     <main className="flex h-screen flex-col bg-[#4440FF] text-white">
-      <nav className="flex items-center justify-between px-56 py-4">
+      <nav className="flex items-center justify-between px-40 py-4">
         <Link href="/">
           <h1 className="text-3xl font-bold">Bandingin</h1>
         </Link>
@@ -26,15 +26,23 @@ const Page = () => {
           />
         </div>
         <div className="flex w-1/2 items-center justify-center">
-          <div className="flex flex-col gap-2">
-            <h2 className="text-3xl font-semibold">
-              Temukan Gadget Impian dalam Sekejap!
-            </h2>
-            <p className="text-lg font-light leading-tight">
-              Bingung pilih gadget yang mau dibeli? Bandingin aja pakai Ai.
-              Pilih dan Ai akan membandingkan 2 gadget biar kamu lebih mudah
-              tentukan pilihan.
-            </p>
+          <div className="flex flex-col gap-8">
+            <div className="flex flex-col gap-1">
+              <h2 className="text-3xl font-semibold">
+                Temukan Gadget Impian dalam Sekejap!
+              </h2>
+              <p className="text-lg font-light leading-tight">
+                Bingung pilih gadget yang mau dibeli? Bandingin aja pakai Ai.
+                Bandingin 2 gadget impian biar kamu lebih mudah tentukan
+                pilihan.
+              </p>
+            </div>
+            <Link
+              href={"/login"}
+              className="w-1/4 rounded-md border px-5 py-1 text-center text-base font-medium hover:bg-[#4946f8]"
+            >
+              Coba Bandingin
+            </Link>
           </div>
         </div>
       </section>
