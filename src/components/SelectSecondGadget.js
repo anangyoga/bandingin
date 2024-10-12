8cd6484e758577f1140069fe5d42006f3238370b
anangyoga <aharwinto@gmail.com>
2024-10-12 23:19:58 +0700

    fix: Select component required & default value disabled

--- src/components/SelectSecondGadget.js
+++ src/components/SelectSecondGadget.js
@@ -4,7 +4,10 @@ const SelectSecondGadget = ({ products }) => {
   return (
     <div className="m-6 flex w-full flex-col items-center rounded-md border bg-white p-2 text-black">
       <div>
-        <select name="secondGadget" className="text-sm font-medium">
+        <select name="secondGadget" className="text-sm font-medium" required>
+          <option value="" selected disabled>
+            Select a gadget
+          </option>
           {products.map((product) => (
             <option key={product.id} value={product.name}>
               {product.name}
