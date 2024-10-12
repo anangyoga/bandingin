e3860f4f02a1d6587ae52fbd4bd9ae7705802e8f
anangyoga <aharwinto@gmail.com>
2024-10-12 16:48:15 +0700

    fix: bandingin page responsive

--- src/app/bandingin/page.js
+++ src/app/bandingin/page.js
@@ -3,32 +3,32 @@ import Link from "next/link";
 const Page = () => {
   return (
     <main className="flex h-screen flex-col bg-[#4440FF] text-white">
-      <nav className="flex items-center justify-between px-40 py-4">
+      <nav className="flex items-center justify-between px-3 py-4 md:px-10 lg:px-40">
         <Link href="/">
-          <h1 className="text-3xl font-bold">Bandingin</h1>
+          <h1 className="text-2xl font-bold sm:text-3xl">Bandingin</h1>
         </Link>
         <div>
           <Link
             href="/login"
-            className="rounded-md border px-5 py-1 text-base font-medium hover:bg-[#4946f8]"
+            className="rounded-md border px-4 py-1 text-base font-medium hover:bg-[#4946f8] sm:px-5 sm:font-medium"
           >
             Login
           </Link>
         </div>
       </nav>
 
-      <section className="mx-auto my-10 flex items-center justify-center sm:max-w-7xl">
-        <div className="flex w-1/2 items-center justify-center">
+      <section className="mx-auto my-10 flex flex-col items-center justify-center sm:max-w-7xl md:flex-row">
+        <div className="flex items-center justify-center md:w-1/2">
           <img
             src="/bandingin.svg"
             alt="Bandingin Image"
             className="aspect-square h-[450px]"
           />
         </div>
-        <div className="flex w-1/2 items-center justify-center">
-          <div className="flex flex-col gap-8">
+        <div className="flex items-center justify-center md:w-1/2">
+          <div className="flex flex-col gap-8 px-3 sm:px-0">
             <div className="flex flex-col gap-1">
-              <h2 className="text-3xl font-semibold">
+              <h2 className="text-2xl font-semibold sm:text-3xl">
                 Temukan Gadget Impian dalam Sekejap!
               </h2>
               <p className="text-lg font-light leading-tight">
@@ -39,7 +39,7 @@ const Page = () => {
             </div>
             <Link
               href={"/login"}
-              className="w-1/4 rounded-md border px-5 py-1 text-center text-base font-medium hover:bg-[#4946f8]"
+              className="rounded-md border px-5 py-1 text-center text-base font-medium hover:bg-[#4946f8] sm:w-1/4"
             >
               Coba Bandingin
             </Link>
