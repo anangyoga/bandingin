bea5ca1989bbbad0ec11538bd7e595d52565a372
anangyoga <aharwinto@gmail.com>
2024-10-12 23:27:37 +0700

    fix: isPending & layouting

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -9,14 +9,14 @@ import Navigation from "./Navigation";
 import GeneratorResult from "./GeneratorResult";
 
 const Generator = ({ products, userData }) => {
-  const [state, action] = useActionState(generateAi, null);
+  const [state, action, isPending] = useActionState(generateAi, null);
 
   return (
-    <div className="flex h-screen flex-col bg-[#4440FF] text-white">
+    <div className="flex h-screen flex-col bg-gray-50 text-black">
       <Navigation userData={userData} />
       <form
         action={action}
-        className="mx-20 flex flex-col items-center rounded-xl border-black p-4"
+        className="flex flex-col items-center border-black p-4"
       >
         <div className="flex gap-20">
           <SelectFirstGadget products={products} />
@@ -24,12 +24,12 @@ const Generator = ({ products, userData }) => {
         </div>
         <button
           type="submit"
-          className="mx-auto flex items-center justify-center gap-x-2 rounded-lg border border-transparent bg-white px-3 py-2 text-sm font-medium text-black hover:opacity-90 focus:outline-none disabled:pointer-events-none disabled:opacity-50 sm:w-1/5"
+          className="mx-auto flex items-center justify-center gap-x-2 rounded-lg border border-transparent bg-blue-600 px-3 py-2 text-sm font-medium text-white hover:opacity-90 focus:outline-none disabled:pointer-events-none disabled:opacity-50 sm:w-1/12"
         >
-          Generate
+          {isPending ? "Generating..." : "Generate"}
         </button>
       </form>
-      {state && <GeneratorResult state={state} />}
+      {state && <GeneratorResult state={state} isPending={isPending} />}
     </div>
   );
 };
