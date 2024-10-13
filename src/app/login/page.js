28c94b66482a3b0fa8f40d2d86343e2b27173a3f
anangyoga <aharwinto@gmail.com>
2024-10-13 13:39:54 +0700

    fix: add copywriting login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -32,43 +32,53 @@ export default function Page() {
         <h1 className="text-lg font-semibold text-white">
           Welcome to Bandingin
         </h1>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-5 pb-2.5">
-          <div className="flex flex-col gap-3">
-            <div className="flex flex-col items-start gap-1">
-              <label className="text-sm text-white">Email</label>
-              <input
-                required
-                type="text"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Email"
-                className="w-full rounded-md border px-2 py-1 text-sm outline-none placeholder:text-sm placeholder:text-slate-400"
-              ></input>
+        <div className="flex flex-col gap-2.5">
+          <p className="self-start text-sm font-medium text-white">
+            Login if you're an admin
+          </p>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-5 pb-2.5">
+            <div className="flex flex-col gap-3">
+              <div className="flex flex-col items-start gap-1">
+                <label className="text-sm text-white">Email</label>
+                <input
+                  required
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Email"
+                  className="w-full rounded-md border px-2 py-1 text-sm outline-none placeholder:text-sm placeholder:text-slate-400"
+                ></input>
+              </div>
+              <div className="flex flex-col items-start gap-1">
+                <label className="text-sm text-white">Password</label>
+                <input
+                  required
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Password"
+                  className="w-full rounded-md border px-2 py-1 text-sm outline-none placeholder:text-sm placeholder:text-slate-400"
+                ></input>
+              </div>
             </div>
-            <div className="flex flex-col items-start gap-1">
-              <label className="text-sm text-white">Password</label>
-              <input
-                required
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-                className="w-full rounded-md border px-2 py-1 text-sm outline-none placeholder:text-sm placeholder:text-slate-400"
-              ></input>
-            </div>
-          </div>
-          <button className="mx-auto flex w-full items-center justify-center gap-x-2 rounded-lg border border-transparent bg-white px-3 py-2 text-sm font-medium text-black hover:opacity-90 focus:bg-gray-300 focus:outline-none disabled:pointer-events-none disabled:opacity-50">
-            Login
-          </button>
-        </form>
+            <button className="mx-auto flex w-full items-center justify-center gap-x-2 rounded-lg border border-transparent bg-white px-3 py-2 text-sm font-medium text-black hover:opacity-90 focus:bg-gray-300 focus:outline-none disabled:pointer-events-none disabled:opacity-50">
+              Login
+            </button>
+          </form>
+        </div>
         <hr />
 
-        <button
-          onClick={() => loginWithGoogle()}
-          className="text-sm font-semibold text-white focus:font-bold focus:outline-none"
-        >
-          Sign in with Google
-        </button>
+        <div className="flex flex-col gap-2.5">
+          <p className="self-start text-sm font-medium text-white">
+            Sign in as user
+          </p>
+          <button
+            onClick={() => loginWithGoogle()}
+            className="text-sm font-semibold text-white focus:font-bold focus:outline-none"
+          >
+            Sign in with Google
+          </button>
+        </div>
       </div>
     </main>
   );
